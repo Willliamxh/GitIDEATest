0cbaf42a18fc66125f0ea5ac5197cc437f93e40a
xuhan.xuhan <xuhan.xuhan@cainiao.com>
2021-08-23 17:43:28 +0800

    add new files

--- untitled/src/main/java/TestJava.java
+++ untitled/src/main/java/TestJava.java
@@ -1,67 +1,22 @@
 import javax.sound.midi.Soundbank;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TestJava {
-    public static long num(String s)
-    {
-        int length=s.length();
-        char chars[]=s.toCharArray();
-        Arrays.sort(chars);
-        HashMap<Character,Integer> map=new HashMap();
-        for(int i=0;i<chars.length;i++)
-        {
-            boolean flag=true;
-            int num=1;
-            while (flag){
-                if(i+1<chars.length&&chars[i]!=chars[i+1]){
-                    if(!(i+1<chars.length))
-                    {
-                        break;
-                    }
-                    i++;
-                    num++;
-                }else {
-                    flag=false;
-                    map.put(chars[i],num);
-                }
-            }
-        }
-        Collection<Integer> values=map.values();
-        //求阶乘
-        long pailieshu=getJC(length);
-        for(int chushu:values)
-        {
-            pailieshu=pailieshu/getJC(chushu);
-        }
-
-        return pailieshu;
-    }
-
-    public static long getJC(int n)
-    {
-        long res=1;
-        for(int i=0;i<n;i++){
-            res=res*(n-i);
-        }
-        return res;
-    }
     public static void main(String[] args) {
-        String a="cc";
-        String b="dd";
-        String c="ccdd";
-        String d=a+b;
-        System.out.println(c==d);
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<String, Integer>(16, 0.75f,true);
+        map.put("星期一", 40);
+        map.put("星期二", 43);
+        map.put("星期三", 35);
+        map.put("星期四", 55);
+        map.put("星期五", 45);
+        map.put("星期六", 35);
+        map.put("星期日", 30);
 
-    }
-    public static long getJieChen(int num) {
-        long result = 1;
-        for (int i = 0; i < num; i++) {
-            result = result * (num-i) ;
-        }
-        return result;
+        System.out.println(map);
+        map.get("星期四");
+        System.out.println(map);
+        String a;
+
     }
 
 }
--- untitled/src/main/java/_123.java
+++ untitled/src/main/java/_123.java
@@ -1,2 +1,7 @@
 public class _123 {
+    public static void main(String[] args) {
+        System.out.println();
+        String a=";";
+        a.equals("");
+    }
 }
